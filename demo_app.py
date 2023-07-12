0f0e03f03c7c544ac0a23388c2cd9c690788f137
Tony Kipkemboi <iamtonykipkemboi@gmail.com>
2023-07-12 05:26:51 -0400

    muuuch berrerrrr

--- demo_app.py
+++ demo_app.py
@@ -1,15 +1,33 @@
 import streamlit as st
+from web3 import Web3
 from src.streamlit_web3_wallet import streamlit_web3_wallet
 
 st.set_page_config("wide")
 
-def main():
-    st.title("Web3 Wallet Integration Demo")
-    value = streamlit_web3_wallet("Connect to Wallet")
-    if value:
-        st.write(f"Wallet address: {value}")
-    else:
-        pass
+alchemy_api_key = st.secrets['ALCHEMY-API-KEY']
+alchemy_url = f"https://eth-mainnet.g.alchemy.com/v2/{alchemy_api_key}"
+w3 = Web3(Web3.HTTPProvider(alchemy_url))
 
-if __name__ == "__main__":
+
+def get_wallet_balance(wallet_address):
+    checksum_address = Web3.to_checksum_address(wallet_address)
+    balance = w3.eth.get_balance(checksum_address)
+    balance_eth = w3.from_wei(balance, 'ether')
+    return balance_eth
+
+
+def main():
+    st.title('💰 Eth Wallet Balance Demo App!')
+
+    if wallet_address := streamlit_web3_wallet():
+        Web3.is_checksum_address(wallet_address)
+        balance = get_wallet_balance(wallet_address)
+        st.success(f'Your wallet balance is: {balance} Ether')
+
+        # Get the latest block number
+        latest_block = w3.eth.block_number
+        st.write(f'Latest block #: {latest_block}')
+
+
+if __name__ == '__main__':
     main()
--- src/streamlit_web3_wallet/frontend/main.js
+++ src/streamlit_web3_wallet/frontend/main.js
@@ -5,65 +5,142 @@ let web3;
 let account = null;
 
 function sendValue(value) {
-  Streamlit.setComponentValue(value);
+  if (typeof value === 'string') {
+    Streamlit.setComponentValue(value);
+  }
 }
 
 // Function to connect to the wallet
 async function connectWallet() {
   if (window.ethereum) {
     web3 = new Web3(window.ethereum);
+
+    // Listen for disconnection:
+    window.ethereum.on('disconnect', (error) => {
+      console.log(`MetaMask Disconnected: ${error}`);
+      // Handle disconnection...
+      disconnectWallet();
+    });
+
     try {
       // Request account access
-      await window.ethereum.enable();
-      const accounts = await web3.eth.getAccounts();
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
       account = accounts[0];
-      document.getElementById('wallet-address').innerText = account;
+      const truncatedAddress = `Connected: ${account.substring(0, 6) + '...' + account.substring(38)}`;
+      document.getElementById('connect-button').innerText = truncatedAddress;
       sendValue(account);
     } catch (error) {
       // User denied account access...
       console.error("User denied account access");
+      document.getElementById('connect-button').innerText = 'Connect Wallet';
     }
-  } 
+
+    // Listen for account changes
+    window.ethereum.on('accountsChanged', (accounts) => {
+      if (accounts.length > 0) {
+        account = accounts[0];
+        const truncatedAddress = `Connected: ${account.substring(0, 6) + '...' + account.substring(38)}`;
+        document.getElementById('connect-button').innerText = truncatedAddress;
+        sendValue(account);
+      } else {
+        // No accounts available, handle as needed
+        disconnectWallet();
+      }
+    });
+  }
   // Legacy dapp browsers...
   else if (window.web3) {
     web3 = new Web3(web3.currentProvider);
-  } 
+  }
   // Non-dapp browsers...
   else {
-    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    console.log('Non-Ethereum browser detected. Please install MetaMask!');
+    document.getElementById('connect-button').innerText = 'Connect Wallet';
   }
 }
 
-// Function to disconnect the wallet
-function disconnectWallet() {
-  web3 = null;
-  account = null;
-  document.getElementById('wallet-address').innerText = 'Not connected';
-  sendValue(null);
+// Function to connect to the wallet
+async function getCurrentConnectedWallet() {
+  if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+
+    // Listen for disconnection:
+    window.ethereum.on('disconnect', (error) => {
+      console.log(`MetaMask Disconnected: ${error}`);
+      // Handle disconnection...
+      disconnectWallet();
+    });
+
+    try {
+      // Request account access
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      account = accounts[0];
+      const truncatedAddress = `Connected: ${account.substring(0, 6) + '...' + account.substring(38)}`;
+      document.getElementById('connect-button').innerText = truncatedAddress;
+      sendValue(account);
+    } catch (error) {
+      // User denied account access...
+      console.error("User denied account access");
+      document.getElementById('connect-button').innerText = 'Connect Wallet';
+    }
+
+    // Listen for account changes
+    window.ethereum.on('accountsChanged', (accounts) => {
+      if (accounts.length > 0) {
+        account = accounts[0];
+        const truncatedAddress = `Connected: ${account.substring(0, 6) + '...' + account.substring(38)}`;
+        document.getElementById('connect-button').innerText = truncatedAddress;
+        sendValue(account);
+      } else {
+        // No accounts available, handle as needed
+        disconnectWallet();
+      }
+    });
+  }
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    web3 = new Web3(web3.currentProvider);
+  }
+  // Non-dapp browsers...
+  else {
+    console.log('Non-Ethereum browser detected. Please install MetaMask!');
+    document.getElementById('connect-button').innerText = 'Connect Wallet';
+  }
+  return null; // Return null if wallet is not connected
 }
 
 function onRender(event) {
   // Only run the render code the first time the component is loaded.
   if (!window.rendered) {
-    // Check if browser has Ethereum capabilities
-    if (typeof window.ethereum === 'undefined') {
-      // Send 'false' to Python
-      sendValue(false);
-    } else {
-      // Attach functions to buttons
-      document.getElementById('connect-button').addEventListener('click', connectWallet);
-      document.getElementById('disconnect-button').addEventListener('click', disconnectWallet);
+    // Attach functions to buttons
+    document.getElementById('connect-button').addEventListener('click', connectWallet);
 
-      // Send 'true' to Python
-      sendValue(true);
+    // Check if wallet is already connected
+    const storedAddress = getCurrentConnectedWallet();
+    if (storedAddress) {
+      // Check if wallet is still connected
+      web3 = new Web3(window.ethereum);
+      web3.eth.getAccounts().then((accounts) => {
+        if (accounts.length > 0 && accounts[0] === storedAddress) {
+          account = storedAddress;
+          const truncatedAddress = `Connected: ${account.substring(0, 6) + '...' + account.substring(38)}`;
+          document.getElementById('connect-button').innerText = truncatedAddress;
+          sendValue(account);
+        } else {
+          disconnectWallet();
+        }
+      });
+    } else {
+      document.getElementById('connect-button').innerText = 'Connect Wallet';
     }
+
     window.rendered = true;
   }
 }
 
-// Render the component whenever python send a "render event"
-Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
+// Render the component whenever python sends a "render event"
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
 // Tell Streamlit that the component is ready to receive events
-Streamlit.setComponentReady()
+Streamlit.setComponentReady();
 // Render with the correct height, if this is a fixed-height component
-Streamlit.setFrameHeight(100)
+Streamlit.setFrameHeight(100);
